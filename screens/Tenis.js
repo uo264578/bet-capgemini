fbcfe7a73a414d22f7003d5ae387635f3d87e1df
Pedro Garcia <uo264578@uniovi.es>
2023-11-14 10:41:10 +0100

    Update Tenis.js

--- screens/Tenis.js
+++ screens/Tenis.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { app } from '../database/firebase';
 import { getFirestore, collection, onSnapshot, getDocs } from 'firebase/firestore';
 
@@ -8,6 +8,40 @@ const partidosTenisCollection = collection(firestore, 'PartidosTenis');
 
 const PartidosTenisScreen = () => {
   const [partidosTenis, setPartidosTenis] = useState([]);
+  const [monto, setMonto] = useState('');
+  const [cuota, setCuota] = useState('');
+  const [equipoSeleccionado, setEquipoSeleccionado] = useState('');
+
+  const handleApostar = (equipo,c) => {
+    setEquipoSeleccionado(equipo);
+    setCuota(c);
+  };
+
+  const handleConfirmarApuesta = () => {
+    const usuarioId = firebase.auth().currentUser.uid;
+
+    // Validar la cantidad apostada
+    const parsedMonto = parseFloat(monto);
+    if (isNaN(parsedMonto) || parsedMonto <= 0) {
+      Alert.alert('Cantidad inválida', 'Ingrese una cantidad válida para apostar.');
+      return;
+    }
+
+    // Realizar la apuesta usando la cantidad, cuota y otros detalles
+    firebase.database().ref(`/apuestas/${usuarioId}`).push({
+      monto: parsedMonto,
+      cuota: parseFloat(cuota),
+      fecha: new Date().toString(),
+      ganador: equipoSeleccionado,
+      ganancia: parsedMonto * cuota, // Ganancia es igual a la cantidad apostada en este caso
+      idUsuario: usuarioId,
+    });
+
+    // Reiniciar el estado después de la apuesta
+    setMonto('');
+    setCuota('');
+    setEquipoSeleccionado('');
+  };
 
   useEffect(() => {
     const obtenerPartidosTenis = async () => {
@@ -22,59 +56,121 @@ const PartidosTenisScreen = () => {
     };
 
     obtenerPartidosTenis();
-    console.log(partidosTenisCollection);
-    const unsubscribe = onSnapshot(partidosTenisCollection, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        // Actualiza el estado con el nuevo dato en tiempo real
-        setPartidosTenis((prevPartidosTenis) => {
-          const updatedPartidosTenis = [...prevPartidosTenis];
-          const changedPartido = change.doc.data();
-          const index = updatedPartidosTenis.findIndex(
-            (partido) => partido.id === change.doc.id
-          );
+    //console.log(partidosTenisCollection);
+    // const unsubscribe = onSnapshot(partidosTenis, (snapshot) => {
+    //   snapshot.docChanges().forEach((change) => {
+    //     // Actualiza el estado con el nuevo dato en tiempo real
+    //     setPartidosTenis((prevPartidosTenis) => {
+    //       const updatedPartidosTenis = [...prevPartidosTenis];
+    //       const changedPartido = change.doc.data();
+    //       const index = updatedPartidosTenis.findIndex(
+    //         (partido) => partido.id === change.doc.id
+    //       );
 
-          if (change.type === 'added') {
-            // Añade el nuevo partido
-            updatedPartidosTenis.push(changedPartido);
-          } else if (change.type === 'modified') {
-            // Actualiza el partido existente
-            updatedPartidosTenis[index] = changedPartido;
-          } else if (change.type === 'removed') {
-            // Elimina el partido existente
-            updatedPartidosTenis.splice(index, 1);
-          }
+    //       if (change.type === 'added') {
+    //         // Añade el nuevo partido
+    //         updatedPartidosTenis.push(changedPartido);
+    //       } else if (change.type === 'modified') {
+    //         // Actualiza el partido existente
+    //         updatedPartidosTenis[index] = changedPartido;
+    //       } else if (change.type === 'removed') {
+    //         // Elimina el partido existente
+    //         updatedPartidosTenis.splice(index, 1);
+    //       }
 
-          return updatedPartidosTenis;
-        });
-      });
-    });
+    //       return updatedPartidosTenis;
+    //     });
+    //   });
+    // });
 
-    return () => {
-      unsubscribe();
-    };
+    // return () => {
+    //   unsubscribe();
+    // };
   }, []);
 
   return (
     <View>
-      <Text>Lista de Partidos de Tenis</Text>
-      <FlatList
-        data={partidosTenis}
-        keyExtractor={(item, index) => (item.id ? item.id.toString() : index.toString())}
-        renderItem={({ item }) => (
-        <View key={item.id}>
-          <Text>Jugador 1: {item.id}</Text>
-          <Text>Jugador 2: {item.jugador2}</Text>
-          <Text>Cuota 1: {item.cuota1}</Text>
-          <Text>Cuota 2: {item.cuota2}</Text>
-          <Text>Fecha: {item.fecha}</Text>
-          <Text>--------------------------</Text>
+    <Text style={{ fontSize: 30, color: 'green' }}>Lista de Partidos de Tenis</Text>
+    <FlatList
+      data={partidosTenis}
+      keyExtractor={(item, index) => (item.id ? item.id : index)}
+      renderItem={({ item }) => (
+        <View key={item.id} style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={{ width:100, fontSize: 20, color: 'blue' }}>{item.Jugador1}</Text>
+          <Text style={{ width: 50, fontSize: 20, color: 'blue' }}> VS </Text>
+          <Text style={{ width: 100, fontSize: 20, color: 'blue' }}>{item.Jugador2}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                equipoSeleccionado === 'Jugador1' && styles.selectedButton,
+                { width: 80, height: 50 },
+              ]}
+              onPress={() => handleApostar('Jugador1', item.Cuota1)}>
+              <Text style={styles.buttonText}>{item.Cuota1}</Text>
+            </TouchableOpacity>
+          
+            <TouchableOpacity
+              style={[
+                styles.button,
+                equipoSeleccionado === 'Jugador2' && styles.selectedButton,
+                { width: 80, height: 50 },
+              ]}
+              a = {item.Cuota2}
+              onPress={() => handleApostar('Jugador2', item.Cuota2)}>
+              <Text style={styles.buttonText}>{item.Cuota2}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-  )}
-/>
-
-
-    </View>
+      )}
+    />
+  </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: ' center',
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  selectedButton: {
+    backgroundColor: 'darkblue',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+});
+
 
 export default PartidosTenisScreen;
