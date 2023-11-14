e5f70b3ed693ba84a755e45b51cad69698f6fa71
HugoC <hugicash@gmail.com>
2023-11-14 17:13:08 +0100

    Update CarrerasCaballos.js

--- screens/CarrerasCaballos.js
+++ screens/CarrerasCaballos.js
@@ -1,43 +1,177 @@
-// CarrerasCaballos.js
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect  } from 'react';
+import { View, Text,FlatList, Button, StyleSheet, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { firebase } from '@react-native-firebase/database';
+import { app } from '../database/firebase';
+import { getFirestore, collection, onSnapshot, getDocs } from 'firebase/firestore';
+import { Ionicons } from '@expo/vector-icons'; 
+import Home from './Home';
+import Baloncesto from './Baloncesto';
+import Tenis from './Tenis';
+import Futbol from './Futbol';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function CarrerasCaballos() {
+const firestore = getFirestore(app);
+const CarrerasCaballosCollection = collection(firestore, 'CarrerasCaballos');
+const Stack = createStackNavigator();
+
+export default function CarrerasCaballos(navigation) {
+  const [carrerasCaballos, setCarrerasCaballos] = useState([]);
   const [monto, setMonto] = useState('');
+  const [cuota, setCuota] = useState('');
   const [caballoSeleccionado, setCaballoSeleccionado] = useState('');
 
-  const handleApostar = () => {
-    const usuarioId = firebase.auth().currentUser.uid;
-
-    firebase.database().ref(`/apuestas/caballos/${usuarioId}`).push({
-      monto,
-      caballoSeleccionado,
-      fecha: new Date().toString(),
-    });
+  
+  const handleApostar = (caballo,c) => {
+    setCaballoSeleccionado(caballo);
+    setCuota(c);
   };
 
+  const handleConfirmarApuesta = () => {
+    const usuarioId = firebase.auth().currentUser.uid;
+    const db = getDatabase();
+  
+    // Validar la cantidad apostada
+    const parsedMonto = parseFloat(monto);
+    if (isNaN(parsedMonto) || parsedMonto <= 0) {
+      Alert.alert('Cantidad inválida', 'Ingrese una cantidad válida para apostar.');
+      return;
+    }
+  
+    // Referencia a la tabla de apuestas
+    const apuestasRef = ref(db, `/apuesta`);
+  
+    // Crear un nuevo registro de apuesta
+    const nuevaApuesta = {
+      monto: parsedMonto,
+      cuota: parseFloat(cuota),
+      fecha: new Date().toString(),
+      ganador: caballoSeleccionado,
+      ganancia: parsedMonto * cuota,
+      idUsuario: usuarioId,
+    };
+  
+    // Empujar los datos de la nueva apuesta a Firebase
+    push(apuestasRef, nuevaApuesta)
+      .then(() => {
+        // Reiniciar el estado después de la apuesta
+        setMonto('');
+        setCuota('');
+        setCaballoSeleccionado('');
+        // Agregar aquí cualquier otra lógica después de realizar la apuesta
+      })
+      .catch((error) => {
+        console.error('Error al crear la apuesta:', error);
+      });
+  };
+
+  useEffect(() => {
+    const obtenerCaballo = async () => {
+      try {
+        const querySnapshot = await getDocs(CarrerasCaballosCollection);
+        const carrerasCaballosData = querySnapshot.docs.map((doc) => doc.data());
+        console.log(carrerasCaballosData);
+        setCarrerasCaballos(carrerasCaballosData);
+      } catch (error) {
+        console.error('Error al leer datos de partidosTenis:', error);
+      }
+    };
+
+    obtenerCaballo();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Página de Apuestas de Carreras de Caballos</Text>
-      <Text style={styles.label}>Ingrese la información de su apuesta:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Monto"
-        keyboardType="numeric"
-        value={monto}
-        onChangeText={(text) => setMonto(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Caballo seleccionado"
-        value={caballoSeleccionado}
-        onChangeText={(text) => setCaballoSeleccionado(text)}
-      />
-      <Button title="Apostar" onPress={handleApostar} />
-    </View>
+    <View>
+    <Text style={{ fontSize: 30, color: 'green' }}>Lista de caballos a correr
+    <Ionicons name="md-tennisball" size={35} color="#FFBB00" />
+    </Text>
+    
+    <FlatList
+      data={carrerasCaballos}
+      keyExtractor={(item, index) => (item.id ? item.id : index)}
+      renderItem={({ item }) => (
+        <View key={item.id} style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <Text style={{ width:100, fontSize: 20, color: 'blue' }}>{item.NombreCaballo1}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                caballoSeleccionado === item.NombreCaballo1 && styles.selectedButton,
+                { width: 80, height: 30 },
+              ]}
+              onPress={() => handleApostar(item.NombreCaballo1, item.Cuota1)}>
+              <Text style={styles.buttonText}>{item.Cuota1}</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={{ width:100, fontSize: 20, color: 'blue' }}>{item.NombreCaballo2}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                caballoSeleccionado === item.NombreCaballo2 && styles.selectedButton,
+                { width: 80, height: 30 },
+              ]}
+              onPress={() => handleApostar(item.NombreCaballo2, item.Cuota2)}>
+              <Text style={styles.buttonText}>{item.Cuota1}</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={{ width:100, fontSize: 20, color: 'blue' }}>{item.NombreCaballo3}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                caballoSeleccionado === item.NombreCaballo3 && styles.selectedButton,
+                { width: 80, height: 30 },
+              ]}
+              onPress={() => handleApostar(item.NombreCaballo3, item.Cuota3)}>
+              <Text style={styles.buttonText}>{item.Cuota1}</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={{ width:100, fontSize: 20, color: 'blue' }}>{item.NombreCaballo4}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[
+                styles.button,
+                caballoSeleccionado === item.NombreCaballo4 && styles.selectedButton,
+                { width: 80, height: 30 },
+              ]}
+              onPress={() => handleApostar(item.NombreCaballo4, item.Cuota4)}>
+              <Text style={styles.buttonText}>{item.Cuota1}</Text>
+            </TouchableOpacity>
+          </View>
+         
+          
+        </View>
+        
+      )}
+    />
+     {caballoSeleccionado !== '' && (
+        <>
+          <Text style={{ fontSize: 20 }}>Ganador: {caballoSeleccionado}</Text>
+          <Text style={{ fontSize: 18 }}>Cuota:</Text>
+          <Text style={{ fontSize: 20 }}>{cuota}</Text>
+          <Text style={{ fontSize: 20 }}>Ingrese la cantidad apostada:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Cantidad"
+            keyboardType="numeric"
+            value={monto}
+            onChangeText={(text) => setMonto(text)} 
+          /> 
+          <Text style={{ fontSize: 20 }}>Posibles ganancias: {cuota * monto} €</Text>
+          <Button title="Confirmar Apuesta" onPress={handleConfirmarApuesta} color="#841584"/>
+        </>
+      )}
+      <View style={styles.bottomContainerAbajo}>
+        <Button title="Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Baloncesto" onPress={() => navigation.navigate('Baloncesto')} />
+        <Button title="Futbol" onPress={() => navigation.navigate('Futbol')} />
+        <Button title="Tenis" onPress={() => navigation.navigate('Tenis')} />
+      </View>
+  </View>
+  
   );
-}
+  
+};
 
 const styles = StyleSheet.create({
   container: {
